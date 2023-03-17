098e69e03693715542be1d2fca25249b9c1de9e2
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-03-17 14:05:18 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,40 @@
 // Copyright 2021 NNTU-CS
-
+void bubleSort(int* arr, int size) {
+    int temp = 0;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+ bubleSort(arr, size);
+    int colvo = 0, left = 0;
+    int right = size - 1;
+        while (left <= right) {
+        int center = (left + right) / 2;
+        if (arr[center] == value) {
+            colvo++;
+            int i = center - 1;
+            while (i >= 0 && arr[i] == value) {
+                colvo++;
+                i--;
+            }
+            i = center + 1;
+            while (i < size && arr[i] == value) {
+                colvo++;
+                i++;
+            }
+            return colvo;
+        } else if (arr[center] < value)
+            left = center + 1;
+        else {
+            right = center - 1;
+        }
+    }
+  return 0;
 }
